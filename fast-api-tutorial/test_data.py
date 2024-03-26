4d54aa5137eeb3d67d26dedfd1c6f60c2bcf34a1
Ivan Sliepov <isliepov@magicleap.com>
2024-03-26 15:13:48 +0100

    added-fast-api-methods

---
+++ fast-api-tutorial/test_data.py
@@ -0,0 +1,12 @@
+class TestData:
+    """Contains different test data, including test pages"""
+
+    # Address of the VirtualBox running server.
+    # The VirtualBox instance IP is defined in the 'hosts' file
+    # of the host machine
+    VM_SERVER_ONE = "http://mytestvmone.com/"
+
+    # Address of the VirtualBox running server.
+    # The VirtualBox instance IP is defined in the 'hosts' file
+    # of the host machine
+    VM_SERVER_TWO = ""
