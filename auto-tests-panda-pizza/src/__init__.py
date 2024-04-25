1b6b3d193d79a698ace71bd10951fb9ef68c635d
Ivan Sliepov <isliepov@magicleap.com>
2024-04-25 13:11:38 +0200

    updated-src-init-comment

--- auto-tests-panda-pizza/src/__init__.py
+++ auto-tests-panda-pizza/src/__init__.py
@@ -1,5 +1,5 @@
 # IF YOU DO NOT WANT TO ENCOUNTER THE ModuleNotFind error,
-# either list all the modules inside the root package('src' in this case)
+# either list all the modules inside the root package('src' in this case) - STILL DOES NOT WORK WITHOUT SETUP!!!
 # OR create the setup.py file according to https://www.youtube.com/watch?v=Mgp6-ZMEcE0
 
 from src.features import environment
